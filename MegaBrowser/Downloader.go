cce6ef2b9aae4ac09613905031e7f96a6216ba17
Mic-Cie <micciesl3@gmail.com>
2024-01-19 20:08:16 +0100

    Adding create directory if not exists func

--- MegaBrowser/Downloader.go
+++ MegaBrowser/Downloader.go
@@ -1,7 +1,9 @@
 package megabrowser
 
 import (
+	"io/fs"
 	"os"
+	"path/filepath"
 
 	"github.com/t3rm1n4l/go-mega"
 )
@@ -12,13 +14,16 @@ type Downloader interface {
 
 type MegaDownloader struct {
 	removeFile removeFileFunc
+	mkDir      mkdirFunc
 }
 
 type removeFileFunc func(path string) error
+type mkdirFunc func(path string, perm fs.FileMode) error
 
 func NewMegaDownloader() *MegaDownloader {
 	return &MegaDownloader{
 		removeFile: os.Remove,
+		mkDir:      os.MkdirAll,
 	}
 }
 
@@ -43,4 +48,25 @@ func (md *MegaDownloader) removeOutdatedFile(localDownloadPath string) error {
 		}
 	}
 	return nil
+}
+
+func (md *MegaDownloader) createFileDirectoryIfNotExist(downloadPathWithFile string) error {
+	fileDirectory := extractDirectoryFromFullPath(downloadPathWithFile)
+	_, err := os.Stat(fileDirectory)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return err
+		}
+	} else {
+		return nil
+	}
+	err = md.mkDir(fileDirectory, 0777)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func extractDirectoryFromFullPath(fullPath string) string {
+	return filepath.Dir(fullPath)
 }
